4f92fc000388617a1396cd2063fbb6ec4586cd33
Toni <alfred.dc.alpha@gmail.com>
2021-07-05 22:47:18 +0200

    Project initiation

---
+++ src/test/java/com/shortener/cutter/CutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortener.cutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
